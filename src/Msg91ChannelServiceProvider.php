416399382e5bdd423b951a1ab2c0bbf92d9d2c75
Ashish Suthar <asissuthar@gmail.com>
2020-02-24 21:17:23 +0530

    Update Msg91ChannelServiceProvider.php

--- src/Msg91ChannelServiceProvider.php
+++ src/Msg91ChannelServiceProvider.php
@@ -11,11 +11,10 @@ class Msg91ChannelServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        Notification::resolved(function (ChannelManager $service) {
+        Notification::resolved(function(ChannelManager $service) {
             $service->extend('otp', function ($app) {
                 return new Channels\Msg91OtpChannel($this->app->make(OtpClient::class));
             });
-
             $service->extend('sms', function ($app) {
                 return new Channels\Msg91SmsChannel($this->app->make(SmsClient::class));
             });
